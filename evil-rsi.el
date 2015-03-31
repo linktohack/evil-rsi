840954c01e9ea01a3dfd88e386c85ab23acbb62c
Quang-Linh LE <linktohack@gmail.com>
2015-03-31 16:25:17 +0200

    Enable C-n and C-p if auto-complete is loaded

--- evil-rsi.el
+++ evil-rsi.el
@@ -63,11 +63,12 @@
 	    (evil-define-key 'insert map "\C-f" 'forward-char)
 	    (evil-define-key 'motion map "\C-k" 'kill-line)
 	    (evil-define-key 'insert map "\C-k" 'kill-line)
-	    (when (auto-complete-mode)
-	      (setq ac-use-menu-map)
-	      (evil-define-key 'insert map "\C-n" 'next-line)
-	      (evil-define-key 'insert map "\C-p" 'previous-line))
             map))
+
+(eval-after-load 'auto-complete
+  '(progn
+     (evil-define-key 'insert evil-rsi-mode-map "\C-n" 'next-line)
+     (evil-define-key 'insert evil-rsi-mode-map "\C-p" 'previous-line)))
 
 (provide 'evil-rsi)
 
