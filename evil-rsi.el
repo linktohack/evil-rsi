8bbbd6eebd0a1947c0173ae5bf0f2af168510238
Quang-Linh LE <linktohack@gmail.com>
2015-04-30 17:28:23 +0200

    Bump to v0.0.2

--- evil-rsi.el
+++ evil-rsi.el
@@ -4,7 +4,7 @@
 
 ;; Author: Quang Linh LE <linktohack@gmail.com>
 ;; URL: http://github.com/linktohack/evil-rsi
-;; Version: 0.0.1
+;; Version: 0.0.2
 ;; Keywords: evil rsi evil-rsi
 ;; Package-Requires: ((evil "1.0.0"))
 
